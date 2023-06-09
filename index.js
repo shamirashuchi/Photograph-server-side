fbf9c442eaf355c77041e68e426f563a85bb413d
shamirashuchi <shamirashuchi@gmail.com>
2023-06-09 10:35:40 +0600

    user-data-added-in-database

--- index.js
+++ index.js
@@ -46,6 +46,20 @@ async function run() {
       res.send(result);
     });
 
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+
     app.post('/selects', async (req, res) => {
       const item = req.body;
       console.log(item);
